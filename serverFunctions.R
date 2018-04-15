6645a144f357edb4b3ffc0e91dada77e5b05bc34
djmlewis <djmlewis@mac.com>
2018-04-15 10:15:01 +0100

    looks nice

--- serverFunctions.R
+++ serverFunctions.R
@@ -583,7 +583,7 @@ handleClick <- function(data,click,cid,facet,fact,yv) {
   }
 
   if(is.null(res) == FALSE && nrow(res) > 0) {
-    showNotification(apply(res[1,], 1, paste, collapse="  --   "), duration = 30, id = cid)
+    showNotification(apply(res[1,], 1, paste, collapse="   ❖   "), duration = 30, id = cid)
   } else {
     removeNotification(id = cid)
   }
